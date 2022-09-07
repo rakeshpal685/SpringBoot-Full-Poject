2599d3d0d0a99d8ada853d3b6f4a6569e1375225
rakeshpal685 <rakesh.pal685@gmail.com>
2022-09-07 21:54:44 +0800

    Initial commit

---
+++ src/main/java/com/java/fullProject/FullProjectApplication.java
@@ -0,0 +1,13 @@
+package com.java.fullProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/fullProject/FullProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.fullProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
